044527c1f6378c26482186cc348f0ac372adeb2e
Anagha Srinath <44865019+anaghasunkadh@users.noreply.github.com>
2026-06-19 11:42:52 -0400

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        dp=[-1]*n
+        dp[0]=nums[0]
+        if len(nums)>1:
+            dp[1]=max(nums[0],nums[1])
+        for i in range(2,len(nums)):
+            if dp[i]!=-1:
+                return dp[i]
+            pick=nums[i]+dp[i-2]
+            notpick=dp[i-1]
+            dp[i]=max(pick,notpick)
+        return dp[n-1]
+        
+        
