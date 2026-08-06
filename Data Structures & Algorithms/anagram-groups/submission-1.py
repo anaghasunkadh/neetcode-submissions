1a8939784a2297d188710dd8a4b6b65ed37e4daf
Anagha Srinath <44865019+anaghasunkadh@users.noreply.github.com>
2026-08-06 16:37:03 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group={}
+        def isAna(A):
+            dict={}
+            for char in A:
+                if char not in dict:
+                    dict[char]=0
+                dict[char]+=1
+            return dict
+        for s in strs:
+            count=isAna(s)
+            key=tuple(sorted(count.items()))
+            if key not in group:
+                group[key]=[]
+            group[key].append(s)
+        return list(group.values())
+
+        
